f3fe22125bcf200fe9e9cd7b59a9c0649cce606e
stusaurus <kiyo0625puma@yahoo.co.jp>
2026-09-02 08:56:38 +0900

    Rebalance starter stages for deliberate moves

--- stages.js
+++ stages.js
@@ -5,9 +5,9 @@ window.STEGON_STAGES = [
     maxMoves: 6,
     goal: { type: "collect", tile: "grass", count: 6 },
     initialBoard: [
-      "grass", "berry", "grass",
       "berry", "berry", "grass",
-      "grass", "berry", "berry"
+      "berry", "grass", "berry",
+      "grass", "berry", "grass"
     ],
     refill: ["grass", "berry", "grass", "berry", "berry", "grass", "grass", "berry"]
   },
@@ -15,12 +15,12 @@ window.STEGON_STAGES = [
     id: 2,
     size: 4,
     maxMoves: 8,
-    goal: { type: "collect", tile: "berry", count: 10 },
+    goal: { type: "collect", tile: "berry", count: 14 },
     initialBoard: [
       "grass", "berry", "grass", "berry",
       "berry", "grass", "berry", "grass",
-      "grass", "grass", "berry", "berry",
-      "berry", "grass", "grass", "berry"
+      "grass", "berry", "grass", "berry",
+      "berry", "grass", "berry", "grass"
     ],
     refill: ["berry", "grass", "berry", "grass", "grass", "berry", "berry", "grass"]
   },
@@ -28,13 +28,13 @@ window.STEGON_STAGES = [
     id: 3,
     size: 5,
     maxMoves: 10,
-    goal: { type: "collect", tile: "grass", count: 16 },
+    goal: { type: "collect", tile: "grass", count: 28 },
     initialBoard: [
+      "grass", "grass", "berry", "grass", "berry",
+      "berry", "grass", "berry", "grass", "berry",
       "grass", "berry", "grass", "berry", "grass",
       "berry", "grass", "berry", "grass", "berry",
-      "grass", "grass", "berry", "berry", "grass",
-      "berry", "grass", "grass", "berry", "berry",
-      "grass", "berry", "berry", "grass", "grass"
+      "grass", "berry", "grass", "berry", "grass"
     ],
     refill: ["grass", "berry", "berry", "grass", "berry", "grass", "grass", "berry", "grass", "berry"]
   }
